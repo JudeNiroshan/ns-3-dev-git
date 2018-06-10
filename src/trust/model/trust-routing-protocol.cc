5a704fe1b7f74fb2bf39ce9cf565b0b55800658b
AnuGlimmersky <anuks3000@gmail.com>
2018-06-10 12:24:57 +0530

    removed a cout

--- src/trust/model/trust-routing-protocol.cc
+++ src/trust/model/trust-routing-protocol.cc
@@ -2344,7 +2344,6 @@ RoutingProtocol::RecvTrr (Ipv4Address sender, Ptr<Packet> packet )
   if (IsMyOwnAddress (trrHeader.GetOrigin()))
   {
 	  std::cout << "TRR BACK TO HOME!!! "<< std::endl;
-	  std::cout << trrHeader.GetTrrLifetime() << std::endl;
 //	  rec = sendTRR(node, targetNode);
 	  Time time (MilliSeconds (trrHeader.GetTrrLifetime()));
 	  Time currentTime = Simulator::Now();
